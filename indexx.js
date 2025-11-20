cf2e091b2f51a4f38eeb1147dd06edbd99be2178
lub5155 <vtarun2004@gmail.com>
2025-11-20 14:07:24 +0530

    ...

--- indexx.js
+++ indexx.js
@@ -8,7 +8,10 @@ function getImage(event) {
     reader.onload = function() {
         var base64Image = reader.result;   // this is the base64 image
         console.log(base64Image);
-        document.getElementById("upld").innerHTML+=`<button class="storyimgbtn"><img class="storyimg" src=${base64Image}></button>`
+        document.getElementById("upld").innerHTML+=`<div>
+        <button class="storyimgbtn"><img class="storyimg" src="${base64Image}"></button>
+        </div>
+        `
     };
     reader.readAsDataURL(file);
 }
