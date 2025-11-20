d045452c51c215b0b65f856dbe1d72b204121c27
lub5155 <vtarun2004@gmail.com>
2025-11-20 13:55:50 +0530

    .

--- indexx.js
+++ indexx.js
@@ -1,3 +1,14 @@
 function inpp(){
     document.getElementById("file").click()
+    document.getElementById("file").addEventListener("change", getImage);
+}
+function getImage(event) {
+    var file = event.target.files[0];  // user selected image
+    var reader = new FileReader();
+    reader.onload = function() {
+        var base64Image = reader.result;   // this is the base64 image
+        console.log(base64Image);
+        document.getElementById("upld").innerHTML+=`<button class="storyimgbtn"><img class="storyimg" src=${base64Image}></button>`
+    };
+    reader.readAsDataURL(file);
 }
